b49f351cec3ba23e34d2d586e5501b3ae6925184
NKragh <39466838+NKragh@users.noreply.github.com>
2018-09-18 13:36:14 +0200

    Initial commit

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Hest
+    {
+        // hestens navn?
+        public String navn;
+        //maxhastighed måles i km/t
+        public double maxHastighed;
+        //fødselsår
+        public int year;
+
+        public Hest()
+        {
+            Console.WriteLine("Indtast venligst hestenavn #1:");
+            navn = Console.ReadLine();
+            Console.WriteLine("Hestens fødselsår?");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hvor hurtig er du");
+            maxHastighed = Convert.ToDouble(Console.ReadLine());
+
+        }
+
+        private void Eat()
+        {
+            Console.WriteLine("Gnaske gnaske...");
+        }
+
+        public void Run()
+        {
+            Console.Write("Wrinske wrinske");
+            Console.WriteLine(" - med " + maxHastighed + " km/t");
+            Console.WriteLine("Wrinske-hilsen " + navn);
+            Console.WriteLine("Jeg er " + (2018 - year) + "år gammel");
+            Eat();
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create instance of  Hest
+            Hest minHest = new Hest();
+            Hest minAndenHest = new Hest();
+
+            minHest.Run();
+            minAndenHest.Run();
+        }
+    }
+}
